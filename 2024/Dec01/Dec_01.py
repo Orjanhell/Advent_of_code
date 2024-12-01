7cc0705c9e9e83a22f6dc9482b6086a7eab9432e
Ørjan <helland.orjan@gmail.com>
2024-12-01 21:58:17 +0100

    work in progress

--- 2024/Dec01/Dec_01.py
+++ 2024/Dec01/Dec_01.py
@@ -1,1 +1,17 @@
 
+with open('note.txt', 'r') as file:
+    lines = file.readlines()
+
+left_numbers = []
+right_numbers = []
+
+for line in lines:
+    numbers = line.strip().split()
+    left_numbers.append(int(numbers[0]))
+    right_numbers.append(int(numbers[1]))
+
+sorted_left = sorted(left_numbers)
+sorted_right = sorted(right_numbers)
+
+print("Sorterte tall på venstere side", sorted_left)
+print("Sorterte tall på høyre side", sorted_right)
