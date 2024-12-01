44e18bae27fbf3b8bbadae7a2f01a45aab7d7a16
Ørjan <helland.orjan@gmail.com>
2024-12-01 22:51:27 +0100

    Nope, was not done. Part 2!

--- 2024/Dec01/Dec_01.py
+++ 2024/Dec01/Dec_01.py
@@ -1,4 +1,3 @@
-
 with open('note.txt', 'r') as file:
     lines = file.readlines()
 
@@ -13,15 +12,14 @@ for line in lines:
 sorted_left = sorted(left_numbers)
 sorted_right = sorted(right_numbers)
 
-print("Sorted numbers left side", sorted_left)
-print("Sorted number right side", sorted_right)
+print("Sorted numbers on the left side:", sorted_left)
+print("Sorted numbers on the right side:", sorted_right)
 
-differences = []
+similarity_score = 0
 
-for left, right in zip(sorted_left, sorted_right):
-    differences.append(abs(left - right))
+for left in sorted_left:
+    count_in_right = right_numbers.count(left)
+    print(f"Number {left} appears {count_in_right} times in the right list.")
+    similarity_score += left * count_in_right
 
-total_distance = sum(differences)
-
-print("Differences in distance", differences)
-print("Total distance", total_distance)
+print("Similarity score between the lists:", similarity_score)
